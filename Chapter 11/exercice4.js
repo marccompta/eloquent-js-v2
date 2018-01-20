f2d398e39d0773e914420a536c7dbe1916b13329
Unknown <marc.compta@gmail.com>
2018-01-20 14:13:17 +0100

    Added solutions to chapter 11, exercices 2 and 4.

---
+++ Chapter 11/exercice4.js
@@ -0,0 +1,27 @@
+specialForms["set"] = function(args, env) {
+    if(args.length != 2 || args[0].type != "word")
+    {
+        throw new SyntaxError("Bad use of set");
+    }
+
+    var name = args[0].name;
+    var value = evaluate(args[1], env);
+
+    return privateSet(args, env);
+
+    function privateSet(args, env) {
+        if(Object.prototype.hasOwnProperty.call(env, name))
+        {
+            env[name] = value;
+            return value;
+        }
+
+        var prototype = Object.getPrototypeOf(env);
+        if(prototype)
+        {
+            return privateSet(args, prototype);
+        }
+
+        throw new ReferenceError("Setting undefined variable: " + name)
+    }
+}
